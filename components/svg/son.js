1ce2fe6ee0d18d5173e4369c52ad008c63d47777
Permanent Record <mklersy33@gmail.com>
2022-04-19 23:14:10 +0200

    patch son icon

--- components/svg/son.js
+++ components/svg/son.js
@@ -2,29 +2,26 @@ import React from "react";
 
 export default function SonIcon() {
     return (
-        <svg width="40px" height="28px" viewBox="0 0 40 28" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-            <defs>
-                <linearGradient x1="59.5701555%" y1="0%" x2="37.3108856%" y2="96.2306308%" id="linearGradient-x5v91exvfv-1">
-                    <stop stopColor="#43063F" offset="0%"></stop>
-                    <stop stopColor="#4D7193" offset="57.8129%"></stop>
-                    <stop stopColor="#6381D0" offset="100%"></stop>
-                </linearGradient>
-                <linearGradient x1="14.6442145%" y1="-3.76838407%" x2="44.8021371%" y2="117.751863%" id="linearGradient-x5v91exvfv-2">
-                    <stop stopColor="#43063F" offset="0%"></stop>
-                    <stop stopColor="#4D7193" offset="57.8129%"></stop>
-                    <stop stopColor="#6381D0" offset="100%"></stop>
-                </linearGradient>
-            </defs>
-            <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                <g id="Group">
-                    <g id="Ellipse-35" transform="translate(29.783813, 7.603035)" stroke="url(#linearGradient-x5v91exvfv-1)" strokeLinecap="round" strokwidth="8">
-                        <path d="M8.8817842e-16,12.54056 C2.74324,12.09266 5.48645,9.73326 5.48645,6.27026 C5.48645,2.8073 2.74324,0.44788 8.8817842e-16,8.8817842e-16" id="Path"></path>
-                    </g>
-                    <g id="Vector-61" fill="url(#linearGradient-x5v91exvfv-2)" fillRule="nonzero">
-                        <path d="M19.948763,0.160893255 L3.657193,7.07077526 C1.440023,8.01115526 -4.4408921e-16,10.1860953 -4.4408921e-16,12.5944953 L-4.4408921e-16,14.8337953 C-4.4408921e-16,17.2878953 1.494483,19.4945953 3.773303,20.4052953 L19.987463,26.8853953 C21.301163,27.4104953 22.729763,26.4428953 22.729763,25.0282953 L22.729763,13.4813953 L22.729763,2.00212526 C22.729763,0.569521255 21.267663,-0.398493745 19.948763,0.160893255 Z" id="Path"></path>
-                    </g>
-                </g>
-            </g>
-        </svg>
+
+<svg width="40px" height="28px" viewBox="0 0 40 28" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+    <defs>
+        <linearGradient x1="59.5703299%" y1="0%" x2="37.3107874%" y2="96.2306308%" id="linearGradient-k16nhkacuj-1">
+            <stop stopColor="#43063F" offset="0%"></stop>
+            <stop stopColor="#4D7193" offset="57.8129%"></stop>
+            <stop stopColor="#6381D0" offset="100%"></stop>
+        </linearGradient>
+        <linearGradient x1="14.6442108%" y1="-3.76838953%" x2="44.8021366%" y2="117.751864%" id="linearGradient-k16nhkacuj-2">
+            <stop stopColor="#43063F" offset="0%"></stop>
+            <stop stopColor="#4D7193" offset="57.8129%"></stop>
+            <stop stopColor="#6381D0" offset="100%"></stop>
+        </linearGradient>
+    </defs>
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <g id="Group-93">
+            <path d="M29.783763,20.1435938 C32.527063,19.6956938 35.270263,17.3362938 35.270263,13.8732938 C35.270263,10.4102938 32.527063,8.05091385 29.783763,7.60303385" id="Path" stroke="url(#linearGradient-k16nhkacuj-1)" strokeWidth="8" strokeLinecap="round"></path>
+            <path d="M19.948763,0.160893846 L3.657193,7.07077385 C1.440023,8.01115385 0,10.1860938 0,12.5944938 L0,14.8337938 C0,17.2878938 1.494483,19.4945938 3.773303,20.4052938 L19.987463,26.8853938 C21.301163,27.4104938 22.729763,26.4428938 22.729763,25.0282938 L22.729763,13.4813938 L22.729763,2.00212385 C22.729763,0.569523846 21.267663,-0.398495154 19.948763,0.160893846 Z" id="Path" fill="url(#linearGradient-k16nhkacuj-2)" fillRule="nonzero"></path>
+        </g>
+    </g>
+</svg>
     )
 }
